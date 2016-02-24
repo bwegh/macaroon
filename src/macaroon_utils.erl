346f32ef9682dfaf85a17c7d32f3780bcd6f0795
Bas Wegh <bas.wegh@kit.edu>
2016-02-24 10:29:24 +0000

    catch the possibility of having bad data

--- src/macaroon_utils.erl
+++ src/macaroon_utils.erl
@@ -6,6 +6,7 @@
          parse_kv/2,
          inspect_kv_list/1,
          bin_to_hex/1,
+         safe_hex_to_bin/1,
          hex_to_bin/1
         ]).
 
@@ -73,26 +74,30 @@ parse_kv(Data,KVList,MapToAtom) ->
     
 
 parse_single_kv(Data,MapToBin) ->
-	case byte_size(Data) >= ?PACKET_PREFIX of 
-		true ->
-			<<LengthEnc:?PACKET_PREFIX/binary,Rest/binary>> = Data,
-			<<LengthA:16/unsigned>> = hex_to_bin(binary:bin_to_list(LengthEnc)),
-			Length = LengthA - ?PACKET_PREFIX,
-			case byte_size(Rest) >= Length of
-				true -> 
-					<<Enc:Length/binary,DataLeft/binary>> = Rest,
-					[BinKey,Val] = binary:split(Enc,[<<" ">>],[trim]),
-                    Key = map_to_atom_key(BinKey,MapToBin),
-                    SList = binary:split(Val,[<<"\n">>],[trim,{scope,{byte_size(Val),-1}}]),
-                    Value = case SList of 
-                                [V] -> V;
-                                [] -> <<>>
-                            end,
-                    
-					{ok,{Key,Value},DataLeft};
-				false ->
-					{error,not_enough_data}
-			end;
+    case byte_size(Data) >= ?PACKET_PREFIX of 
+        true ->
+            <<LengthEnc:?PACKET_PREFIX/binary,Rest/binary>> = Data,
+            case safe_hex_to_bin(LengthEnc) of
+                {ok, <<LengthA:16/unsigned>>} ->
+                    Length = LengthA - ?PACKET_PREFIX,
+                    case byte_size(Rest) >= Length of
+                        true -> 
+                            <<Enc:Length/binary,DataLeft/binary>> = Rest,
+                            [BinKey,Val] = binary:split(Enc,[<<" ">>],[trim]),
+                            Key = map_to_atom_key(BinKey,MapToBin),
+                            SList = binary:split(Val,[<<"\n">>],[trim,{scope,{byte_size(Val),-1}}]),
+                            Value = case SList of 
+                                        [V] -> V;
+                                        [] -> <<>>
+                                    end,
+
+                            {ok,{Key,Value},DataLeft};
+                        false ->
+                            {error,not_enough_data}
+                    end;
+                {error,_} ->
+                    {error, not_valid_kv}
+            end;
 		false ->
 			{error,not_enough_data}
 	end.
@@ -134,6 +139,15 @@ bin_to_hex(<<>>,Hex) ->
 bin_to_hex(<<C:8,Rest/binary>>,Hex) ->
 	CHex = list_to_binary(io_lib:format("~2.16.0b",[C])),
 	bin_to_hex(Rest,<< Hex/binary, CHex/binary >>).
+
+safe_hex_to_bin(Hex) ->
+    try hex_to_bin(Hex) of
+        Bin -> {ok, Bin}
+    catch _:_ ->
+              {error, hex_to_bin}
+    end.
+
+
 
 hex_to_bin(BinaryHex) when is_binary(BinaryHex) ->
     hex_to_bin(binary_to_list(BinaryHex));
